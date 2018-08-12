efe7bdd356d757332d95e4cd3dfefd097dd5d9f9
TimurSirkin <timur_sirkin@mail.ru>
2018-08-12 21:34:28 +0300

    Добавил классы Table Lesson Day Class

---
+++ SchoolTT_02/Table/Day.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolTT_02.Table
+{
+    /// <summary>
+    /// Логика взаимодействия для Day.xaml
+    /// </summary>
+    public partial class Day : UserControl
+    {
+        public Day()
+        {
+            InitializeComponent();
+        }
+
+
+        //<Поля>
+        List<Lesson> LessonList = new List<Lesson>();//Список уроков в этот день
+        //</Поля>
+
+        //<Методы>
+        void Add(Lesson pLesson)
+        {
+            this.LessonList.Add(pLesson);
+            TableGrid.RowDefinitions.Add(new RowDefinition());
+            TableGrid.Children.Add(pDay);
+            Grid.SetRow(pDay, pPlace);
+            TableGrid.RowDefinitions.Add(new RowDefinition());
+        }
+        //</Методы>
+
+        //<Обработчики>
+
+        //</Обработчики>
+    }
+}
--- SchoolTT_02/Table/Table.xaml.cs
+++ SchoolTT_02/Table/Table.xaml.cs
@@ -24,5 +24,40 @@ namespace SchoolTT_02.Table
         {
             InitializeComponent();
         }
+
+
+
+        //<Поля>
+        List<Day> DayList = new List<Day>();//Список дней в таблице
+
+        List<Class> ClassList = new List<Class>();//Список классов в таблице
+        //</Поля>
+
+        //<Методы>
+        void Add(Day pDay, int pPlace)//Добавление дня в список и создание новой строки
+        {
+            this.DayList.Add(pDay);
+            TableGrid.RowDefinitions.Add(new RowDefinition());
+            TableGrid.Children.Add(pDay);
+            Grid.SetRow(pDay, pPlace);
+            TableGrid.RowDefinitions.Add(new RowDefinition());
+        }
+
+        void Add(Class pClass, int pPlace)//Добавление класса в список и создание нового столбца
+        {
+            this.ClassList.Add(pClass);
+        }
+        //</Методы>
+
+        //<Обработчики>
+        private void AddClassClick(object sender, RoutedEventArgs e)
+        { 
+        }//Обработчик нажатия кнопки добавления класса
+
+        private void AddDayClick(object sender, RoutedEventArgs e)
+        {
+            Add(new Day(), TableGrid.RowDefinitions.Count);
+        }//Обработчик нажатия кнопки добавления дня
+        //</Обработчики>
     }
 }
